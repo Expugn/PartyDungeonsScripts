51f2b5141f431922e5e484ea9934d56f7529d137
S'pugn <Expugn@users.noreply.github.com>
2021-11-04 22:37:54 -0700

    AlbeyDungeon: Fix Minor Bug
    - Hall of Fame would print a console error if it is empty.

--- AlbeyDungeon/scripts/interact/-1500_170_-1011.js
+++ AlbeyDungeon/scripts/interact/-1500_170_-1011.js
@@ -14,9 +14,13 @@
  */
 function main() {
     const ChatColor = Java.type("org.bukkit.ChatColor");
-    player.sendMessage(`${ChatColor.GRAY}The following is a list of players who have completed the\ndungeon in ${ChatColor.RED}IMPOSSIBLE ${ChatColor.GRAY}difficulty. Congratulations!`);
     const dungeon = sm.getDungeon(player);
     const hall_of_fame = dungeon.getDungeon().getDungeonFile().getVariable("hall_of_fame");
+    if (!hall_of_fame) {
+        player.sendMessage(`${ChatColor.GRAY}Nobody is in the ${ChatColor.GOLD}Hall of Fame ${ChatColor.GRAY}yet. To be added, you must complete the dungeon in ${ChatColor.RED}IMPOSSIBLE ${ChatColor.GRAY}difficulty.`);
+        return;
+    }
+    player.sendMessage(`${ChatColor.GRAY}The following is a list of players who have completed the\ndungeon in ${ChatColor.RED}IMPOSSIBLE ${ChatColor.GRAY}difficulty. Congratulations!`);
     for (const obj of hall_of_fame) {
         player.sendMessage(`${ChatColor.DARK_GRAY}- ${ChatColor.GOLD}${obj[1]} ${ChatColor.GRAY}(${new Date(obj[2]).toString()})`);
     }
